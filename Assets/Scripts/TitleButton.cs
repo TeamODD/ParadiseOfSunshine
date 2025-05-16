b7c45ab5bdf4a72d3d50db40eeeea944720e15cd
GhostRamen918 <143938358+GhostRamen918@users.noreply.github.com>
2025-05-17 01:40:13 +0900

    타이틀

---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnStart()
+    {
+        //인트로 씬 이름
+        //SceneManager.LoadScene("");
+    }
+}
